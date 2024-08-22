908a000952c377f69d2869bd8dcb22ceadad8b18
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2024-08-22 12:43:37 -0300

    feat: Adiciona componente de Loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,13 @@
+import './LoadingStyle.css';
+
+const Loading = () => {
+    return (
+        <div className="loading-container">
+            <div className="loading-dot"/>
+            <div className="loading-dot"/>
+            <div className="loading-dot"/>
+        </div>
+    )
+}
+
+export default Loading;
